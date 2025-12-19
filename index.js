ed8322ec46a2ed3f7176a2f49a7554718fa23fe0
priya <priyankatathe4@gmail.com>
2025-12-19 12:32:51 +0530

    allow frontend access

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const app = express()
 app.use(express.json())
 app.use(cookieparser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+    "http://localhost:5173",
+    "https://event-frontend-1z6w.onrender.com"
+  ],
     credentials: true
 }))
 
